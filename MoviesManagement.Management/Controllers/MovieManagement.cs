e13e6e207e0843c55e6e093576682128ec43e7bf
Hoang-Minh <quataoxanh83@gmail.com>
2019-05-10 23:51:04 -0700

    Add MovieDto and GenreDto

---
+++ MoviesManagement.Management/Controllers/MovieManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesManagement.Management.Controllers
+{
+    public class MovieManagement
+    {
+        //public IEnumerable<MovieDto>
+    }
+}
