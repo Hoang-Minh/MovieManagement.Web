349dcaa5f386486594659bcc42b959186ffd6629
Hoang-Minh <quataoxanh83@gmail.com>
2019-05-11 17:30:23 -0700

    Add Rental form html

---
+++ MoviesManagement.Web/Controllers/RentalsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoviesManagement.Management.Controllers;
+using MoviesManagement.Web.ViewModels;
+
+namespace MoviesManagement.Web.Controllers
+{
+    public class RentalsController : Controller
+    {
+        // GET: Rentals
+        public ActionResult Index()
+        {
+            var customerManagement = new CustomerManagement();
+            var customers = customerManagement.GetCustomersWithMembershipTypes();
+            
+            var movieManagement = new MovieManagement();
+            var movies = movieManagement.GetAllMoviesWithGenres();
+            var rentalFormViewModel = new RentalFormViewModel
+            {
+                Customers = customers,
+                Movies = movies
+            };
+
+            return View(rentalFormViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MoviesManagement.Web/ViewModels/RentalFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MoviesManagement.DataContract;
+
+namespace MoviesManagement.Web.ViewModels
+{
+    public class RentalFormViewModel
+    {
+        public IEnumerable<CustomerDto> Customers { get; set; }
+        public IEnumerable<MovieDto> Movies { get; set; }
+    }
+}
